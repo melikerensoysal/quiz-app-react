b8af5e383290e8c64bb609e2082f294386b27c74
melikerensoysal <melikerensoysal@gmail.com>
2025-10-20 17:51:36 +0300

    chore: add remaining project files and initial setup

---
+++ quiz-app/src/main.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./styles/main.scss";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1, 
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
