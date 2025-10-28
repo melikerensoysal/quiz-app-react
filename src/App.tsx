97ec47b7d62be8ea0ab204f76477874fcd18558d
melikerensoysal <melikerensoysal@gmail.com>
2025-10-28 11:42:32 +0300

    fix: remove unused ResultPage import to fix Vercel build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import QuizPage from "./pages/QuizPage/QuizPage";
-import ResultPage from "./pages/ResultPage/ResultPage";
 
 function App() {
   return (
