b12bb4273c6ecac03e653bb89a4b9ae75991d2c0
melikerensoysal <melikerensoysal@gmail.com>
2025-11-06 15:47:40 +0300

    fix: corrected import paths for app and modal components

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
-import App from "./app.tsx";
+import App from "./app";
 import "./styles/main.scss";
 
 const queryClient = new QueryClient();
